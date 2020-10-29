58e5781318c166db641ec5c067394f75b1387e11
marco <goethe@marco-fuchs.de>
2020-10-29 12:16:26 +0100

    Initial commit

---
+++ src/main/java/tv/strohi/stfu/playlistservice/StfuPlaylistServiceApplication.java
@@ -0,0 +1,13 @@
+package tv.strohi.stfu.playlistservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StfuPlaylistServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StfuPlaylistServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tv/strohi/stfu/playlistservice/StfuPlaylistServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package tv.strohi.stfu.playlistservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StfuPlaylistServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
