3917d8f34f5b7cd645e16c370e55eca2004491e2
marco <goethe@marco-fuchs.de>
2020-10-30 19:03:50 +0100

    add client controller

---
+++ src/main/java/tv/strohi/stfu/playlistservice/controllers/ClientController.java
@@ -0,0 +1,47 @@
+package tv.strohi.stfu.playlistservice.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import tv.strohi.stfu.playlistservice.data.model.Client;
+import tv.strohi.stfu.playlistservice.data.repository.ClientRepository;
+
+import java.util.stream.StreamSupport;
+
+@RestController
+@RequestMapping("clients")
+public class ClientController {
+    private ClientRepository clientRepo;
+
+    @Autowired
+    public void setClientRepo(ClientRepository repo) {
+        clientRepo = repo;
+    }
+
+    @GetMapping
+    public Client[] getAllClients() {
+        return StreamSupport.stream(clientRepo.findAll().spliterator(), false).toArray(Client[]::new);
+    }
+
+    @GetMapping("{id}")
+    public Client getClient(@PathVariable("id") long id) {
+        return clientRepo.findById(id).orElse(null);
+    }
+
+    @PostMapping
+    public Client addClient(@RequestBody Client newClient) {
+        clientRepo.save(newClient);
+        return newClient;
+    }
+
+    @DeleteMapping
+    public String removeAllClients() {
+        clientRepo.deleteAll();
+        return "Alle Clients wurden gelöscht.";
+    }
+
+    @DeleteMapping("{id}")
+    public String removeClient(@PathVariable("id") long id) {
+        clientRepo.deleteById(id);
+        return String.format("Client wurde gelöscht. %s Elemente befinden sich im Repo.", StreamSupport.stream(clientRepo.findAll().spliterator(), false).count());
+    }
+}
